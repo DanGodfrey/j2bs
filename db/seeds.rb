a6ae86b142c2dbc9c4a24058b4986454b82e8e42
Godfrey <godan@109add4155b8.(none)>
2011-06-26 00:36:16 -0700

    shotened seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,16 +10,9 @@ J2BsRequest.destroy_all
 
 req1 = J2BsRequest.create(:order_id => '111',:customer_id => '222', 
   :store_id => '333',:title => 'Three Wolf Moon Shirt' ,:price => 25, 
-  :description => "This Internet sensation has been featured on You Tube, 
-  been discussed in numerous blogs, and has even been the subject of a 
-  song and music video. Now, you can own the legendary Three Wolf Moon 
-  T-shirt! This adult T-shirt features the Three Wolf Moon design, printed 
-  on black tie-dyed 100% cotton.", :status => 'pending' )
+  :description => "This Internet sensation has been featured on You Tube, been discussed in numerous blogs, and has even been the subject of a song and music video.", :status => 'pending' )
   
 req2 = J2BsRequest.create(:order_id => '444',:customer_id => '222', 
   :store_id => '333',:title => 'Dan Bo' ,:price => 121.24, 
-  :description => "Japanese Best Comic Yotsubato! (Published by Asukimediawakusu
-   Kiyo Azuma Kiyohiko), the setting of a robot costume made of cardboard summer 
-   work, Dan Bo. If Amazon is the cardboard box when they shipped? Made the 
-   assumption that [Amazon Riborutekkudanbo boxed mini version appeared.",
+  :description => "Japanese Best Comic Yotsubato! (Published by Asukimediawakusu Kiyo Azuma Kiyohiko), the setting of a robot costume made of cardboard summer work, Dan Bo.",
   :status => 'pending' )
